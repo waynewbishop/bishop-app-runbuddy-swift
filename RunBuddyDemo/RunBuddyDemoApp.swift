2ebcb137c8535c8f753d7ea5929fca93f03b9c09
Wayne Bishop <waynewbishop@users.noreply.github.com>
2024-04-10 12:27:30 -0700

    Initial Commit

---
+++ RunBuddyDemo/RunBuddyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunBuddyDemoApp.swift
+//  RunBuddyDemo
+//
+//  Created by Wayne Bishop on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct RunBuddyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
