b2a59c1b30efecd0c52b72241ab835dde8fb66bb
Wayne Bishop <waynewbishop@users.noreply.github.com>
2024-06-17 22:44:34 -0700

    Additional renaming..

--- RunBuddyDemo/Views/Favorite/FavoriteView.swift
+++ RunBuddyDemo/Views/Favorite/FavoriteView.swift
@@ -1,18 +1,57 @@
 //
-//  FavoriteView.swift
+//  FavoriteCell.swift
 //  RunBuddyDemo
 //
-//  Created by Wayne Bishop on 6/17/24.
+//  Created by Wayne Bishop on 6/16/24.
 //
 
 import SwiftUI
 
+
 struct FavoriteView: View {
+    
+@Binding var selectedName: String
+var systemIcon: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Button(action: {
+                // Add your action code here, e.g., toggle favorite state
+                print("now loading a favorite location...")
+            }) {
+                Image(systemName: systemIcon)
+                    .font(.system(size: 35, weight: .regular))
+                    .foregroundStyle(.white)
+                    .background(
+                        Circle()
+                            .fill(getRandomColor().opacity(0.4))
+                            .frame(width: 65, height: 65)
+                    )
+            }
+        }
+        .frame(height: 65) // set the desired VStack height
+        VStack {
+            Text(selectedName)
+        }
+        
     }
+    
+    
+    //obtains a random color
+    func getRandomColor() -> Color {
+        let colors: [Color] = [.green, .blue, .yellow, .orange, .red, .purple, .gray]
+        let randomIndex = Int.random(in: 0..<colors.count)
+        return colors[randomIndex]
+    }
+        
 }
 
 #Preview {
-    FavoriteView()
+    
+    @State var name = "Perrigo Park"
+    let systemIcon = "mappin.and.ellipse"
+    
+    return VStack {
+        FavoriteView(selectedName: $name, systemIcon: systemIcon)
+    }
 }
